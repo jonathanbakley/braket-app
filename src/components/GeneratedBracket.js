1146e34695bf238b2adced3eda3f49d80006c86b
Jonathan  Bakley <jonathanbakley@Jonathans-MacBook-Pro.local>
2019-11-13 20:36:17 -0500

    created input boxes for players names

--- src/components/GeneratedBracket.js
+++ src/components/GeneratedBracket.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const GeneratedBracket = () => {
-  return <div>Hello I am the generated bracket</div>;
+const GeneratedBracket = ({ numPlayers }) => {
+  console.log(numPlayers);
+  return (
+    <div>
+      Hello I am the generated bracket
+      {[...Array(numPlayers).keys()].map(key => (
+        <div key={key}>Hello from key {key}</div>
+      ))}
+    </div>
+  );
 };
 
 export default GeneratedBracket;
