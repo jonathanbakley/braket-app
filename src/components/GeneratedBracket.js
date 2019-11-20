20627e4ea679979319be9e26934657dd9b9f98ea
Matthew Bakley <matthewbakley@gmail.com>
2019-11-20 17:06:44 -0500

    added playersArray, removed old array

--- src/components/GeneratedBracket.js
+++ src/components/GeneratedBracket.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const GeneratedBracket = ({ numPlayers }) => {
-  console.log(numPlayers);
+const GeneratedBracket = ({ players }) => {
+  console.log(players);
   return (
     <div>
       Hello I am the generated bracket
-      {[...Array(numPlayers).keys()].map(key => (
+      {[...Array(players.length).keys()].map(key => (
         <div key={key}>Hello from key {key}</div>
       ))}
     </div>
