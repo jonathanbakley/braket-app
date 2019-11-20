25f8de925b1e871029275a38100b9f9697a55dd9
jonathanbakley <jonathanbakley@gmail.com>
2019-11-20 18:43:03 -0500

    begin adding styling

--- src/components/GeneratedBracket.js
+++ src/components/GeneratedBracket.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const GeneratedBracket = ({ players }) => {
-  console.log(players);
+  const onClickHandler = e => {
+    console.log("PLayers:", players);
+  };
   return (
     <div>
       Hello I am the generated bracket
       {[...Array(players.length).keys()].map(key => (
         <div key={key}>Hello from key {key}</div>
       ))}
+      <button onClick={onClickHandler} />
     </div>
   );
 };
